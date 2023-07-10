c6b270e5e71b8d76353371c4c71d3c3463e3d94d
Aniket Hase <ws_htu1260@hoonartek03.net>
2023-07-10 19:28:16 +0530

    Added Table for Enrichment Page

--- src/components/Enrichment.js
+++ src/components/Enrichment.js
@@ -658,7 +658,7 @@ const Enrichment = () => {
                       {handleDate(row.RUN_ID)}
                     </TableCell>
                     <TableCell align="center">
-                      <div className="flex justify-between">
+                      <div className="flex justify-center">
                         {row.STATUS.toLowerCase() === "failed" ||
                         row.STATUS.toLowerCase() === "false" ? (
                           <button
@@ -669,7 +669,7 @@ const Enrichment = () => {
                             //     message: item.ERROR,
                             //   })
                             // }
-                            className="opacity-1 px-2 hover:text-inherit"
+                            className="flex flex-row items-center px-2 justify-center text-red-600"
                             title="Request Error"
                           >
                             <svg
@@ -686,19 +686,22 @@ const Enrichment = () => {
                                 d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
                               />
                             </svg>
+                            <span className="pl-2 underline">Error</span>
                           </button>
                         ) : (
                           <button
                             // onClick={() =>
-                            //   fetchcsvTableData(row.TEMPLATE_NAME, row.RUN_ID)
+                            //   fetchcsvTableData(
+                            //     row.TEMPLATE_NAME,
+                            //     row.RUN_ID
+                            //   )
                             // }
-                            disabled={row.STATUS.toLowerCase() !== "completed"}
                             className={`${
                               row.STATUS.toLowerCase() === "completed"
-                                ? "opacity-1 hover:text-inherit"
-                                : "disabled opacity-10 hover:text-inherit"
-                            }  px-2 hover:text-amaranth-600`}
-                            title="View"
+                                ? "text-deep-navy"
+                                : "text-electric-green/50"
+                            } flex flex-row items-center px-2 justify-center`}
+                            disabled={row.STATUS.toLowerCase() !== "completed"}
                           >
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
@@ -719,34 +722,36 @@ const Enrichment = () => {
                                 d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                               />
                             </svg>
+                            <span className="pl-2 underline">View</span>
                           </button>
                         )}
+
                         <button
                           // onClick={() =>
                           //   downloadFile(row.TEMPLATE_NAME, row.RUN_ID)
                           // }
-                          disabled={row.STATUS.toLowerCase() !== "completed"}
                           className={`${
                             row.STATUS.toLowerCase() === "completed"
-                              ? "opacity-1 hover:text-inherit"
-                              : "disabled opacity-10 hover:text-inherit"
-                          }  px-2 hover:text-amaranth-600`}
-                          title="Download file"
+                              ? "text-deep-navy"
+                              : "text-downriver-400"
+                          } flex flex-row items-center px-2 justify-center`}
+                          disabled={row.STATUS.toLowerCase() !== "completed"}
                         >
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
                             viewBox="0 0 24 24"
-                            strokeWidth="1.5"
+                            strokeWidth={1.5}
                             stroke="currentColor"
                             className="w-5 h-5"
                           >
                             <path
                               strokeLinecap="round"
                               strokeLinejoin="round"
-                              d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
                             />
                           </svg>
+                          <span className="pl-2 underline">Download</span>
                         </button>
                       </div>
                     </TableCell>
